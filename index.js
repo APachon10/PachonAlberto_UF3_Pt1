3e17603643f2b12df6cc909b590917fee4a4a3e8
APachon10 <Pachondestroy95@gmail.com>
2019-02-14 19:57:44 +0100

    Login como Api completado

--- index.js
+++ index.js
@@ -13,27 +13,30 @@ app.set('view engine', 'ejs');
 app.get('/', function (req, res) {
     res.render('Plantilla_Formulario')
 });
-
 app.post('/login', function(req, res) {
   var username = req.body.nombre;
   var password = req.body.pass;
-
   var i =0;
   for(i=0 ;i<diccionario.length;i++){
-    if(username == diccionario[i].user && password == diccionario[i].pass){
-      var mensaje = "Bienvenido     ";
-      var users = diccionario[i].user;
-      var frase_final = mensaje.concat(users);  
-      
-      res.send(frase_final);
+    console.log('Usuario '+diccionario[i].user);
+    if(username == diccionario[i].user && password == diccionario[i].pass){      
+      res.send('Login Correcto ');
       return;
-    }else{
-      res.send("Login Incorrecto");
     }
   }
-
+  res.send("Login Incorrecto");
 });
 
+app.get('/api/login/:username/:password',function(req,res){
+  var i =0;
+  for(i=0 ;i<diccionario.length;i++){
+    if(req.params.username == diccionario[i].user && req.params.password == diccionario[i].pass){      
+      res.send('Bienvenido '+req.params.username);
+      return;
+    }
+  }
+  res.send("Login Incorrecto");
+});
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
