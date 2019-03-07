657dc12e6029b645bc58827ffbb9964c6fd5d771
APachon10 <Pachondestroy95@gmail.com>
2019-03-07 20:02:31 +0100

    prova

--- index.js
+++ index.js
@@ -1,43 +1,10 @@
-var express = require('express');
-var app = express();;
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-var bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded({ extended: false }))
-
-var diccionario = [{user:"Alberto",pass:"abc123"} ,
-                  {user:"Sean",pass:"abc456"},
-                  {user:"Miku",pass:"Miku10"} ];
-
-app.set('view engine', 'ejs');
-
-app.get('/', function (req, res) {
-    res.render('Plantilla_Formulario')
-});
-app.post('/login', function(req, res) {
-  var username = req.body.nombre;
-  var password = req.body.pass;
-  var i =0;
-  for(i=0 ;i<diccionario.length;i++){
-    console.log('Usuario '+diccionario[i].user);
-    if(username == diccionario[i].user && password == diccionario[i].pass){
-      res.send('Login Correcto ');
-      return;
-    }
-  }
-  res.send("Login Incorrecto");
-});
-
-app.get('/api/login/:username/:password',function(req,res){
-  var i =0;
-  for(i=0 ;i<diccionario.length;i++){
-    if(req.params.username == diccionario[i].user && req.params.password == diccionario[i].pass){
-      res.send('Bienvenido '+req.params.username);
-      return;
-    }
-  }
-  res.send("Login Incorrecto");
-});
-var port = process.env.port || 3000;
-app.listen(port, function () {
-  console.log('Example app listening on port ' +port);
-});
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
