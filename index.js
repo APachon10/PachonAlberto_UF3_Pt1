08f4ace5647726de46419aa600076638bc178f25
APachon10 <Pachondestroy95@gmail.com>
2019-05-06 19:40:58 +0200

    App Acabada , Los cambios Anteriores se han quitado debido a un error mio

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var app = express();
 
-var pg = require('pg');
-
 var bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -28,10 +26,9 @@ app.post('/login', function(req, res) {
   }
   res.send("Login Incorrecto");
 });
-
 app.get('/api/login/:username/:password',function(req,res){
   var i =0;
-  for(i=0 ;i<diccionario.length;i++){
+  for(i=0 ;i<diccionario.length;i++){ 
     if(req.params.username == diccionario[i].user && req.params.password == diccionario[i].pass){
       res.send('Bienvenido '+req.params.username);
       return;
@@ -39,18 +36,6 @@ app.get('/api/login/:username/:password',function(req,res){
   }
   res.send("Login Incorrecto");
 });
-app.get('/postgres://wmdqrdbyjlqlwb:3354330f5efcfae09de87be86324eb3825df73d44c4d250ce6b2975bd80b4853@ec2-174-129-240-67.compute-1.amazonaws.com:5432/d3j9of6roliub9
-',async(req, res)
-	 try {
-      const client = await pool.connect()
-      const result = await client.query('SELECT * FROM USERS');
-      
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-}
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Example app listening on port ' +port);
